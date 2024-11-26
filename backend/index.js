1ca32c9f5d9a6906f595309e77bd7a310eff1e8e
kacheri sadak <142469534+sadhakkacheri@users.noreply.github.com>
2024-11-26 11:39:28 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const app = express();
 
 // CORS configuration
 const corsOptions = {
-  origin: 'https://jobportal1-dv0n.onrender.com/', // Frontend URL
+  origin: 'https://jobportal1-dv0n.onrender.com', // Frontend URL
   methods: 'GET, POST, PUT, DELETE', 
   credentials: true,
 };
